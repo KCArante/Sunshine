dff20b62812654b559b9033cb9934bd2a267c642
KCArante <kc.arante@gmail.com>
2016-01-30 13:51:03 -0800

    Implementation of the user interface on the fragment activity

--- app/src/main/java/com/example/android/sunshine/fragments/MainFragment.java
+++ app/src/main/java/com/example/android/sunshine/fragments/MainFragment.java
@@ -19,6 +19,8 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View fragmentView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return fragmentView;
     }
 }
