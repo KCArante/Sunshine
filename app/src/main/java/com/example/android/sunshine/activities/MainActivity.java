8e6da1f148aa2008f637fbd243ff60b5af6a59ae
KCArante <kc.arante@gmail.com>
2016-03-15 10:32:03 -0700

    Lesson 1 on Sunshine Tutorial completed. Creating the sunshine project with the adapter implementation and fragment.

--- app/src/main/java/com/example/android/sunshine/activities/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/activities/MainActivity.java
@@ -1,19 +1,37 @@
 package com.example.android.sunshine.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.android.sunshine.R;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView mlistView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceHolderFragment())
+                    .commit();
+        }
+
+        mlistView = (ListView)findViewById(R.id.listview_forecast);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -39,5 +57,34 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class PlaceHolderFragment extends Fragment {
+
+        ArrayAdapter<String> mForecastAdapter;
+
+        public PlaceHolderFragment(){}
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View fragmentView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            ArrayList<String> weekForecast = new ArrayList<String>();
+            weekForecast.add("Today-Sunny-88/63");
+            weekForecast.add("Tomorrow-Cloudy-25/98");
+            weekForecast.add("Wed-Sunny-88/63");
+            weekForecast.add("Thu-Cloudy-90/87");
+            weekForecast.add("Fri-Rainy-98/63");
+            weekForecast.add("Sat-Cloudy-13/87");
+
+            mForecastAdapter = new ArrayAdapter<String>(getActivity(),
+                    R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+
+            ListView listView = (ListView) fragmentView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
+
+            return fragmentView;
+        }
     }
 }
